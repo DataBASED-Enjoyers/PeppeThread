005557c7ca9f4ca9c17ee90c6021def47afad698
grafvontee <grafvontee@yandex.ru>
2024-10-20 17:03:12 +0300

    rwlock_test.py updated

--- src/task3/rwlock_test.py
+++ src/task3/rwlock_test.py
@@ -4,22 +4,18 @@ import re
 import os
 
 
-def start_benchmark(nthreads, n_runs = 1000):
+def start_benchmark(nthreads, test, n_runs = 1000):
     program_original_name = './pth_ll_rwl.o'
     program_peppe_name = './ppt_ll_rwl.o'
     program_name = program_peppe_name
-    folder_name = 'benchmarks'
+    folder_name = f'benchmarks_{test[0]}_{test[1]}_{test[2]}_{test[3]}'
 
     os.makedirs(folder_name, exist_ok=True)
     csv_file = f'{folder_name}/execution_times_{nthreads}_threads.csv'
 
     execution_times = []
 
-    num_of_keys = 1024
-    num_of_ops = 1024
-    search_rate = 0.3
-    insert_rate = 0.2
-    input_data = '\n'.join(map(str, [num_of_keys, num_of_ops, search_rate, insert_rate])) + '\n'
+    input_data = '\n'.join(map(str, test)) + '\n'
 
     for i in range(n_runs):
         result = subprocess.run([program_name, str(nthreads)], 
@@ -37,8 +33,8 @@ def start_benchmark(nthreads, n_runs = 1000):
             break
         execution_times.append(exec_time)
         
-        if (i+1) % 100 == 0:
-            print(f'Выполнено {i+1} запусков из {n_runs}')
+        # if (i+1) % 100 == 0:
+        #     print(f'Выполнено {i+1} запусков из {n_runs}')
 
     with open(csv_file, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
@@ -59,14 +55,23 @@ if __name__ == '__main__':
 
     S, E = 0, 0
 
-    for n_thread in (1, 2, 8, 16, 64):
-        if n_thread == 1:
-            avg_time_thread_1 = start_benchmark(nthreads=n_thread)
-        else:
-            avg_time_thread_n = start_benchmark(nthreads=n_thread)
-            S = avg_time_thread_1 / avg_time_thread_n
-            E = S / n_thread
+    # test = [num_of_keys, num_of_ops, search_rate, insert_rate]
+    test_1 = [1024, 1024, 0.3, 0.2]
+    test_2 = [2024, 555, 0.3, 0.2]
+    test_3 = [555, 2024, 0.3, 0.2]
+    test_4 = [1024, 1024, 0.5, 0.4]
+    test_5 = [1024, 1024, 0.8, 0.1]
+    test_6 = [1024, 1024, 0.1, 0.1]
 
-            print(f"{E=} | {S=}")
+    for test in (test_1, test_2, test_3, test_4, test_5, test_6):
+        for n_thread in (1, 2, 8, 16, 20, 64):
+            if n_thread == 1:
+                avg_time_thread_1 = start_benchmark(nthreads=n_thread, test=test)
+            else:
+                avg_time_thread_n = start_benchmark(nthreads=n_thread, test=test)
+                S = avg_time_thread_1 / avg_time_thread_n
+                E = S / n_thread
 
-        print('-' * 50)
+                print(f"{E=} | {S=}")
+
+            print('-' * 50)
