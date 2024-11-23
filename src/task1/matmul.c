29d2009306afb95641e391c458a6ce90efa96415
grafvontee <grafvontee@yandex.ru>
2024-11-23 12:59:32 +0300

    added check to task1

--- src/task1/matmul.c
+++ src/task1/matmul.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#define CHECK_RESULTS_CORRECTNESS
+
 void row_split_multiplication(int rank, int size, int n, int *matrix,
                               int *vector, int *result);
 void column_split_multiplication(int rank, int size, int n, int *matrix,
@@ -88,6 +90,109 @@ void block_split_multiplication(int rank, int size, int n, int *matrix,
     free(local_result);
 }
 
+// Checking result correctness
+//
+// Последовательное умножение матрицы на вектор для проверки
+void sequential_multiplication(int n, int *matrix, int *vector, int *result) {
+    for (int i = 0; i < n; i++) {
+        result[i] = 0;
+        for (int j = 0; j < n; j++) {
+            result[i] += matrix[i * n + j] * vector[j];
+        }
+    }
+}
+
+// Функция для сравнения двух массивов
+int compare_results(int n, int *result1, int *result2) {
+    for (int i = 0; i < n; i++) {
+        if (result1[i] != result2[i]) {
+            return 0; // Результаты отличаются
+        }
+    }
+    return 1; // Результаты совпадают
+}
+
+#ifdef CHECK_RESULTS_CORRECTNESS
+int main(int argc, char *argv[]) {
+    int n = atoi(getenv("MAT_SIZE")); // Размер матрицы и вектора
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int *matrix = NULL;
+    int *vector = (int *)malloc(n * sizeof(int));
+    int *result = (rank == 0) ? (int *)malloc(n * sizeof(int)) : NULL;
+    int *sequential_result = (rank == 0) ? (int *)malloc(n * sizeof(int)) : NULL;
+
+    if (rank == 0) {
+        matrix = (int *)malloc(n * n * sizeof(int));
+        for (int i = 0; i < n; i++) {
+            vector[i] = 1; // Заполняем вектор значениями
+            for (int j = 0; j < n; j++) {
+                matrix[i * n + j] = i + j + 1; // Заполняем матрицу
+            }
+        }
+        // Выполняем последовательное умножение
+        sequential_multiplication(n, matrix, vector, sequential_result);
+    }
+
+    double start, end;
+
+    // Проверка для умножения с разбиением по строкам
+    start = MPI_Wtime();
+    row_split_multiplication(rank, size, n, matrix, vector, result);
+    end = MPI_Wtime();
+    if (rank == 0) {
+        printf("Row-split time: %f seconds\n", end - start);
+        if (compare_results(n, result, sequential_result)) {
+            printf("Row-split multiplication is correct.\n");
+        } else {
+            printf("Row-split multiplication is incorrect.\n");
+        }
+    }
+
+    // Проверка для умножения с разбиением по столбцам
+    start = MPI_Wtime();
+    column_split_multiplication(rank, size, n, matrix, vector, result);
+    end = MPI_Wtime();
+    if (rank == 0) {
+        printf("Column-split time: %f seconds\n", end - start);
+        if (compare_results(n, result, sequential_result)) {
+            printf("Column-split multiplication is correct.\n");
+        } else {
+            printf("Column-split multiplication is incorrect.\n");
+        }
+    }
+
+    // Проверка для умножения с разбиением по блокам
+    start = MPI_Wtime();
+    block_split_multiplication(rank, size, n, matrix, vector, result);
+    end = MPI_Wtime();
+    if (rank == 0) {
+        printf("Block-split time: %f seconds\n", end - start);
+        if (compare_results(n, result, sequential_result)) {
+            printf("Block-split multiplication is correct.\n");
+        } else {
+            printf("Block-split multiplication is incorrect.\n");
+        }
+    }
+
+    if (rank == 0) {
+        free(matrix);
+        free(result);
+        free(sequential_result);
+    }
+    free(vector);
+
+    MPI_Finalize();
+    return 0;
+}
+#endif
+//
+// end checking
+
+#ifndef CHECK_RESULTS_CORRECTNESS
 int main(int argc, char *argv[]) {
     int n = atoi(getenv("MAT_SIZE"));
     int rank, size;
@@ -144,3 +249,4 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     return 0;
 }
+#endif
