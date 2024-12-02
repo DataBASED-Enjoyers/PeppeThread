886cb2a767ac5a1c182c66e89d26a6b1911359b8
Kitsu <krasilnikovkolya4654@gmail.com>
2024-12-03 00:41:53 +0300

    benchmarks for task2

--- utils/cannon_matmul_metrics.py
+++ utils/cannon_matmul_metrics.py
@@ -61,9 +61,10 @@ def plot_matrix_performance(data, save_path):
 
 
 def main() -> None:
-    matrix_sizes = [x for x in range(500, 3_000) if x % 4 == 0 and x % 9 == 0][::20]
+    matrix_sizes = [x for x in range(500, 3_000) if x % 4 == 0 and x % 9 == 0][::25]
+    print(matrix_sizes)
     num_processes = [1, 4, 9]
-    num_of_tries = 100
+    num_of_tries = 20
 
     os.makedirs(f'{ROOT_DIR}/src/task2/task_2_benchmark', exist_ok=True)
     for size in matrix_sizes:
@@ -80,7 +81,7 @@ def main() -> None:
                 '**E**']
         )
         for processes in tqdm.tqdm(num_processes, desc=f'[Size={os.environ.get("MAT_SIZE")}]'):
-            results = [run_mpi_program(processes) for _ in range(num_of_tries)]
+            results = [run_mpi_program(processes) for _ in tqdm.trange(num_of_tries)]
             result = sum(results) / num_of_tries
 
             if processes == 1:
